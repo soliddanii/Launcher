11c76d4c574f41c3f6ec0d7435832a878281504b
soliddanii <danielc942@gmail.com>
2023-04-01 15:39:43 +0200

    Hide console on launch by default

--- launcher/src/main/java/com/skcraft/launcher/launch/LaunchProcessHandler.java
+++ launcher/src/main/java/com/skcraft/launcher/launch/LaunchProcessHandler.java
@@ -43,7 +43,7 @@ public class LaunchProcessHandler implements Function<Process, ProcessConsoleFra
                 public void run() {
                     consoleFrame = new ProcessConsoleFrame(CONSOLE_NUM_LINES, true);
                     consoleFrame.setProcess(process);
-                    consoleFrame.setVisible(true);
+                    consoleFrame.setVisible(false);
                     MessageLog messageLog = consoleFrame.getMessageLog();
                     messageLog.consume(process.getInputStream());
                     messageLog.consume(process.getErrorStream());
